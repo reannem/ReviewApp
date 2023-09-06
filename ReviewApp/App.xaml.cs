018d4b9ab6972a2e8e2b01a43043c7157664c0d9
Jcuin <Jcuin@CUINO>
2023-09-06 19:07:50 +0800

    Add project files.

---
+++ ReviewApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ReviewApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ReviewApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ReviewApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
